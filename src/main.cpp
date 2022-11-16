3560b3651eac4cf7f6bb0477c7f458de1e05246f
Didarul Ahasan Redwan <dredwan1@binghamton.edu>
2022-11-15 19:49:33 -0500

    Integrator added to main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
-//#include <igl/opengl/glfw/Viewer.h>
 #include <igl/readOFF.h>
 #include <igl/writeOBJ.h>
 #include <iostream>
 #include <cmath>
 #include <igl/readOFF.h>
-#include <igl/per_face_normals.h>
+#include <igl/writeOFF.h>
+#include <igl/per_vertex_normals.h>
 #include <igl/writeOBJ.h>
 #include <igl/adjacency_list.h>
 #include "igl/unique_edge_map.h"
 #include "igl/doublearea.h"
-#include <iostream>
 #include "igl/oriented_facets.h"
 #include "igl/edge_flaps.h"
 #include "igl/edges.h"
@@ -18,63 +17,70 @@
 #include <igl/cotmatrix.h>
 #include <igl/massmatrix.h>
 #include <igl/invert_diag.h>
-
-#include "energy.h"
+#include <vector>
+#include <igl/adjacency_matrix.h>
+#include <igl/vertex_triangle_adjacency.h>
+#include <igl/gaussian_curvature.h>
+//#include <igl/intrinsic_delaunay_triangulation.h>
 #include "meshops.h"
-
+#include "energy.h"
 int numV;                                               // number of vertices
 int numF;                                               // number of faces
 double area_avg;                                        // average area of each triangle mesh
-Eigen::MatrixXd V;                                      // matrix storing vertice coordinates
-Eigen::MatrixXi F;                                      // matrix storing face information, every face is one row with three integers
-Eigen::MatrixXi TT, TTi;                                // face adjacency information
-Eigen::MatrixXd F_normals;                              // face normals
-Eigen::VectorXd dblA;                                   // store face edge information
-
-Eigen::MatrixXd HN,H;
-Eigen::SparseMatrix<double> L,M,Minv;
-Eigen::VectorXd EB;
-
-
-//double total_bending_energy;
-
-//double totalEB;
+Eigen::MatrixXd V,V_new;                                      // matrix storing vertice coordinates
+Eigen::MatrixXi F;
 
 int main(){
-  // Load a mesh in OFF format
-  igl::readOFF("oblate.off", V, F);
-
-  //Print the vertices and faces matrices
-  //std::cout << "Vertices: " << std::endl << V << std::endl;
-  //std::cout << "Faces:    " << std::endl << F << std::endl;
-//   numF = F.rows();
-    float reduced_volume=0.7;
+    igl::readOFF("oblate.off", V, F);
+    numF = F.rows();
     numV = V.rows();
-    igl::doublearea(V,F,dblA);
-    double area_total = dblA.sum();
-    area_avg = dblA.mean();
-    std::cout << "average double area " << dblA.mean() << std::endl;
-    std::cout << "total area " << dblA.sum()/2 << std::endl;
-/*    Energy E1;
-    double total_bending_energy=E1.BendingEnergy(V,F);
-     //Model m;
-    std::cout << "total_bending_energy" <<total_bending_energy<< std::endl;
-    double area_energy=E1.AreaEnergy(V,F);
-    std::cout << "area_energy" <<area_energy<< std::endl;
-    double volume_energy=E1.VolumeEnergy(V,F,reduced_volume);
-    std::cout << "volume_energy" <<volume_energy<< std::endl;
+    int iterations=10000;
 
-    Mesh M1;
-    double volume= M1.cal_volume(V,F);
-    std::cout << "total_volume" <<volume<< std::endl;
-    Eigen::MatrixXd Area_Grad=M1.area_grad(V,F);
-    std::cout << "Area_Grad_Matrix:" <<Area_Grad<< std::endl;
-*/
-     Mesh M1;
-     Eigen::MatrixXd Volume_Grad=M1.volume_grad(V,F);
-     std::cout << "Volume_Grad_Matrix:" <<Volume_Grad<< std::endl;
-  // Save the mesh in OBJ format
-  //igl::writeOBJ("cube.obj",V,F);
+    Eigen::MatrixXd ForceArea,ForceVolume,ForceBending,velocity,ForceTotal; //forces
+
+    double dt=0.001,time; //time step
+    double gamma=2.0;
+    double tolerance=1.0e-7;
+    float reduced_volume=0.65;
+    double EnergyVolume,EnergyArea,EnergyBending,EnergyTotal,EnergyTotal_new, EnergyChange;  //energies
 
 
+    Energy E1;
+    E1.compute_bendingenergy_force(V,F,ForceBending,EnergyBending);
+    E1.compute_areaenergy_force(V,F,ForceArea,EnergyArea);
+    E1.compute_volumeenergy_force(V,F,reduced_volume,ForceVolume,EnergyVolume);
+    EnergyTotal=EnergyBending+EnergyArea+EnergyVolume;
+    ForceTotal=ForceBending+ForceArea+ForceVolume;
+    std::cout<<"Bending EnergyE \n"<< EnergyBending<<std::endl;
+//    velocity= ForceTotal/gamma;//testing
+    V_new=V;
+//    V_new += velocity*dt;
+  for (int i=0; i< iterations; i++){
+         velocity=ForceTotal/gamma;
+         V_new += velocity*dt;
+         Eigen::MatrixXd l;
+         //igl::edge_lengths(V,F,l);
+         //igl::intrinsic_delaunay_triangulation(l,F,l,F);
+         E1.compute_bendingenergy_force(V_new,F,ForceBending,EnergyBending);
+         E1.compute_areaenergy_force(V_new,F,ForceArea,EnergyArea);
+         E1.compute_volumeenergy_force(V_new,F,reduced_volume,ForceVolume,EnergyVolume);
+         ForceTotal=ForceBending+ForceArea+ForceVolume;
+         EnergyTotal_new=EnergyBending+EnergyArea+EnergyVolume;
+         EnergyChange=abs(EnergyTotal_new-EnergyTotal);
+         EnergyTotal=EnergyTotal_new;
+
+         time +=dt;
+         std::cout<<"time \n "<< time<<"\n iteration \n"<<i<<std::endl;
+         std::cout<<"EnergyChange \n "<<EnergyChange<<std::endl;
+
+
+        if (EnergyChange<tolerance){
+          std::cout<<"Bending EnergyE \n"<< EnergyBending<<std::endl;
+          std::cout<<"Area Energy \n"<< EnergyArea<<std::endl;
+          std::cout<<"Volume Energy \n"<< EnergyVolume<<std::endl;
+          std::cout<<"Total Energy \n"<< EnergyTotal<<std::endl;
+          igl::writeOFF("cube.off",V_new,F);
+          break;
+        }
+    }
 }

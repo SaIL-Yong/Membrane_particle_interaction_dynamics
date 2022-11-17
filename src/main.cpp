053cdfd601f6867e4b574d36418bf7cd3e9ecc83
Didarul Ahasan Redwan <dredwan1@binghamton.edu>
2022-11-17 13:43:35 -0500

    logfrequncy_added_to_main

--- src/main.cpp
+++ src/main.cpp
@@ -30,21 +30,21 @@ double area_avg;                                        // average area of each 
 Eigen::MatrixXd V,V_new;                                      // matrix storing vertice coordinates
 Eigen::MatrixXi F;
 
-int main(){
     igl::readOFF("oblate.off", V, F);
     numF = F.rows();
     numV = V.rows();
-    int iterations=10000;
+    int iterations=20000;
+    int logfrequency=100;
 
     Eigen::MatrixXd ForceArea,ForceVolume,ForceBending,velocity,ForceTotal; //forces
 
-    double dt=0.001,time; //time step
-    double gamma=2.0;
-    double tolerance=1.0e-7;
+    double dt=0.01,time; //time step
+    double gamma=1.0;
+    double tolerance=1.0e-8;
     float reduced_volume=0.65;
     double EnergyVolume,EnergyArea,EnergyBending,EnergyTotal,EnergyTotal_new, EnergyChange;  //energies
 
-
+    Mesh M1;
     Energy E1;
     E1.compute_bendingenergy_force(V,F,ForceBending,EnergyBending);
     E1.compute_areaenergy_force(V,F,ForceArea,EnergyArea);
@@ -52,15 +52,14 @@ int main(){
     EnergyTotal=EnergyBending+EnergyArea+EnergyVolume;
     ForceTotal=ForceBending+ForceArea+ForceVolume;
     std::cout<<"Bending EnergyE \n"<< EnergyBending<<std::endl;
-//    velocity= ForceTotal/gamma;//testing
     V_new=V;
-//    V_new += velocity*dt;
   for (int i=0; i< iterations; i++){
          velocity=ForceTotal/gamma;
          V_new += velocity*dt;
          Eigen::MatrixXd l;
-         //igl::edge_lengths(V,F,l);
-         //igl::intrinsic_delaunay_triangulation(l,F,l,F);
+         igl::edge_lengths(V_new,F,l);
+         V_new=M1.vertex_smoothing(V_new,F);
+         igl::intrinsic_delaunay_triangulation(l,F,l,F);
          E1.compute_bendingenergy_force(V_new,F,ForceBending,EnergyBending);
          E1.compute_areaenergy_force(V_new,F,ForceArea,EnergyArea);
          E1.compute_volumeenergy_force(V_new,F,reduced_volume,ForceVolume,EnergyVolume);
@@ -73,8 +72,19 @@ int main(){
          std::cout<<"time \n "<< time<<"\n iteration \n"<<i<<std::endl;
          std::cout<<"EnergyChange \n "<<EnergyChange<<std::endl;
 
+        if(i%logfrequency ==0){
+          std::cout<<"time \n "<< time<<"\n iteration \n"<<i<<std::endl;
+          std::cout<<"EnergyChange \n "<<EnergyChange<<std::endl;
+          std::cout<<"Bending EnergyE \n"<< EnergyBending<<std::endl;
+          std::cout<<"Area Energy \n"<< EnergyArea<<std::endl;
+          std::cout<<"Volume Energy \n"<< EnergyVolume<<std::endl;
+          std::cout<<"Total Energy \n"<< EnergyTotal<<std::endl;
+
+        }
+
 
         if (EnergyChange<tolerance){
+          std::cout<<"Change of Energy is very small \n Reached Equilibrioum Shape"<<std::endl;
           std::cout<<"Bending EnergyE \n"<< EnergyBending<<std::endl;
           std::cout<<"Area Energy \n"<< EnergyArea<<std::endl;
           std::cout<<"Volume Energy \n"<< EnergyVolume<<std::endl;
@@ -84,3 +94,4 @@ int main(){
         }
     }
 }
+

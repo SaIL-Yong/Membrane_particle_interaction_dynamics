6aa07207ff2d51bc46f4bf91695816035e1d3683
Xin Yong <xyong@ece-1094487-d.bu.binghamton.edu>
2023-02-28 17:02:16 -0500

    add MATLAB vis script

---
+++ visualization/mesh_Visualization.m
@@ -0,0 +1,86 @@
+clc
+clear all
+close all
+
+fid1=fopen('equilibrium.off');
+
+while feof(fid1)==0
+    temp=fgetl(fid1);
+    temp=fgetl(fid1);
+    [nV,nF,nE]=strread(temp, '%d %d %d');
+    
+    FV.vertices=zeros(nV,3);
+    FV.faces=zeros(nF,3);
+    
+    for i=1:nV
+        temp=fgetl(fid1);
+        vertex=sscanf(temp, '%g %g %g');
+        x(i)=vertex(1);
+        y(i)=vertex(2);
+        z(i)=vertex(3);    
+    end
+     x=x';
+     y=y';
+     z=z';
+    
+    for i=1:nF
+        temp=fgetl(fid1);
+        tri(i,:)=sscanf(temp, '%*d %d %d %d %*d %*d');
+    end
+    tri=tri+1;
+
+end
+fclose(fid1);
+
+fid2=fopen('logfile.txt');
+
+while feof(fid2)==0
+    for i = 1:17
+        temp=fgetl(fid2);
+    end
+    if (strcmp(sscanf(temp,"%*s %*s %s"),"outside"))
+        particle_position = 1;
+    elseif (strcmp(sscanf(temp,"%*s %*s %s"),"inside"))
+        particle_position = -1;
+    end
+    temp=fgetl(fid2);
+    R0 = sscanf(temp,"%*s %*s %f, %f, %f");
+    temp=fgetl(fid2);
+    Rp = sscanf(temp,"%*s %*s %f");
+
+    for i = 1:6
+        temp=fgetl(fid2);
+    end
+    chi = sscanf(temp,"%*s %*s %*s %f");
+    break;
+end
+fclose(fid2);
+
+F1=figure;
+set(gcf,'Position',[50 50 1000 600])
+hold on;
+h = 2*chi;
+Rpp = Rp - 0.01;
+[Rx,Ry,Rz] = sphere(200);
+X1 = Rpp*Rx; Y1 = Rpp*Ry; Z1 = Rpp*Rz;
+X2 = Rpp*Rx; Y2 = Rpp*Ry; Z2 = Rpp*Rz;
+if (particle_position > 0)
+    X1(Rz>-1+h) = NaN; Y1(Rz>-1+h) = NaN; Z1(Rz>-1+h) = NaN;
+    X2(Rz<=-1+h) = NaN; Y2(Rz<=-1+h) = NaN; Z2(Rz<=-1+h) = NaN;
+else
+    X1(Rz>1-h) = NaN; Y1(Rz>1-h) = NaN; Z1(Rz>1-h) = NaN;
+    X2(Rz<=1-h) = NaN; Y2(Rz<=1-h) = NaN; Z2(Rz<=1-h) = NaN;
+end
+
+S1=surf(X1+R0(1),Y1+R0(2),Z1+R0(3));
+set(S1,'FaceColor',"#A2142F", ...
+  'FaceAlpha',1.0,'FaceLighting','gouraud','EdgeColor','none');
+S2=surf(X2+R0(1),Y2+R0(2),Z2+R0(3));
+set(S2,'FaceColor',"#0072BD", ...
+  'FaceAlpha',1.0,'FaceLighting','gouraud','EdgeColor','none');
+pp = patch('Faces',tri,'Vertices',[x,y,z],'FaceColor',"#EDB120",'FaceLighting','gouraud','FaceAlpha',0.2,'EdgeColor','k');
+axis equal;
+axis([-1.5 1.5 -1.5 1.5 -2.0 2.0]);
+view(90,0);
+%lightangle(45,30);
+title(['R_p = ', num2str(Rp), ' \chi = ', num2str(chi)]);

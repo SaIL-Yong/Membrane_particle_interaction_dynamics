22c151752956b4d256fbb4e2a49978eb4856f1bd
Redwan0521 <dredwan1@binghamton.edu>
2025-08-19 14:45:10 -0400

    deleted_old_files

---
+++ manuscript_cases/spherical_vesicle_cubical_particle/u=4.0/job_batch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#SBATCH --job-name=u=4.0_spherical
+#SBATCH --output=u=4.0.log
+#Number of processors
+#SBATCH --time=10-00:00:0
+#SBATCH --nodes=1
+#Number of cores
+#SBATCH --ntasks-per-node=1
+##SBATCH --exclusive
+#SBATCH --mail-user=didarula@buffalo.edu
+#SBATCH --mem=12G
+#SBATCH --partition=xinyong
+#SBATCH --qos=xinyong
+#SBATCH --cluster=faculty
+./MemDynamics
